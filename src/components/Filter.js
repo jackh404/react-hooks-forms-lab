07d3b37e2998d66f26ef383c037dfa731685e1ee
Jack Henza <jackhenza@gmail.com>
2023-10-27 15:05:22 -0400

    filters by controlled inputs

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({
+  onCategoryChange,
+  selectedCategory,
+  onSearchChange,
+  keyWord,
+}) {
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        onChange={onSearchChange}
+        value={keyWord}
+      />
+      <select
+        name="filter"
+        onChange={onCategoryChange}
+        value={selectedCategory}
+      >
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,23 +5,38 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [keyWord, setKeyWord] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function onSearchChange(event) {
+    setKeyWord(event.target.value);
+  }
 
-    return item.category === selectedCategory;
+  const itemsToDisplay = items.filter(item => {
+    console.log("does " + item.name + " contain " + keyWord);
+    console.log(item.name.toLowerCase().includes(keyWord.toLowerCase()));
+    if (
+      (selectedCategory === "All" || item.category === selectedCategory) &&
+      item.name.toLowerCase().includes(keyWord.toLowerCase())
+    ) {
+      return true;
+    }
+    return false;
   });
-
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        selectedCategory={selectedCategory}
+        onSearchChange={onSearchChange}
+        keyWord={keyWord}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {itemsToDisplay.map(item => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
